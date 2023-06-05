102f85ad41305bc09a284b7425a13090d562b2e9
Samuel Balanlay <balanlaysc@gmail.com>
2023-06-05 23:02:53 +0800

    start button functionality

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ let elapsedTime = 0;
 let current = 0;
 let paused = true;
 let intervalId;
-let millis = 0;
 let hrs = 0;
 let mins = 0;
 let secs = 0;
+let millis = 0;
+
+startBtn.addEventListener("click", () => {
+  if (paused) {
+    paused = false;
+
+    startTime = Date.now() - elapsedTime;
+    intervalId = setInterval(updateTime, 1);
+  }
+});
+pauseBtn.addEventListener("click", () => {});
+resetBtn.addEventListener("click", () => {});
+
+function updateTime() {
+  elapsedTime = Date.now() - startTime;
+
+  millis = Math.floor(elapsedTime % 1000);
+  secs = Math.floor((elapsedTime / 1000) % 60);
+  mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+  hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+
+  timeDisplay.textContent = `${hrs}:${mins}:${secs}.${millis}`;
+}
